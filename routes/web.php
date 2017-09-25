02bcb6c26e089ee947fec34bd62ad71d9722107d
Joel Baluma <baluma.joel91@gmail.com>
2017-09-25 10:31:54 +0800

    updates

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,8 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
-Route::resource('users', 'UsersController');
-Route::get('all-users', 'UsersController@all');
+Route::group(['middleware' => ['auth']], function () {
+	Route::get('/home', 'HomeController@index')->name('home');
+	Route::resource('users', 'UsersController');
+	Route::get('all-users', 'UsersController@all');
+});
