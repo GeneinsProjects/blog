91a2d31cd5cd4420a90be427c6fbf02d119b27af
Genein Merced L. Lumayag <geneinlumayag@gmail.com>
2017-09-27 00:42:19 +0800

    fred update with cloud

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>NetTech</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
@@ -83,7 +83,7 @@
                     Genein NETTECH Fred
                     <div class="row" style="text-align:center;">
 
-                    <img src="{{asset('cloud.jpg')}}" style="width: 300px;">
+                    <img src="{{asset('cloud.png')}}" style="width: 1000px;">
                     </div>
 
 
